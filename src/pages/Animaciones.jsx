7c011b5de52e63317f4fc0fe4e4d900fa71cc254
ChriisLM <christianlezcano878@gmail.com>
2024-05-14 21:34:29 -0500

    Estructuracion de la pagina animaciones

--- src/pages/Animaciones.jsx
+++ src/pages/Animaciones.jsx
@@ -5,14 +5,31 @@ function Animaciones() {
     return (
         <>
             <Header />
-            <section className="container">
-                <div className="title-container">
+            <article className="container">
+                <section className="title-container">
                     <h1 className="title">Animaciones</h1>
-                </div>
-                <div className="projects-container">
-
-                </div>
-            </section>
+                </section>
+                <section className="projects-container">
+                    <div className="container-animacion1 card"></div>
+                    <div className="container-animacion2 card"></div>
+                    <div className="container-animacion3 card"></div>
+                    <div className="container-animacion4 card"></div>
+                    <div className="container-animacion5 card"></div>
+                    <div className="container-animacion6 card"></div>
+                    <div className="container-animacion7 card"></div>
+                    <div className="container-animacion8 card"></div>
+                    <div className="container-animacion9 card"></div>
+                    <div className="container-animacion10 card"></div>
+                    <div className="container-animacion11 card"></div>
+                    <div className="container-animacion12 card"></div>
+                    <div className="container-animacion13 card"></div>
+                    <div className="container-animacion14 card"></div>
+                    <div className="container-animacion15 card"></div>
+                    <div className="container-animacion16 card"></div>
+                    <div className="container-animacion17 card"></div>
+                    <div className="container-animacion18 card"></div>
+                </section>
+            </article>
         </>
     )
 }
